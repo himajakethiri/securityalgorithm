651f44c446c3c569a7f75606b85c2df6c691917a
himajakethiri <kethirihimajareddy@gmail.com>
2016-05-04 08:38:16 -0400

    Update Driver.java

--- Final Project/Driver.java
+++ Final Project/Driver.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
  * File: Driver.java 
  * author: Himaja Kethiri, Sandeep Reddy Salla 
  * course:Security Algorithms and Protocols
- * Project: AES Encryption and Decryption for 128, 192 and 256 bits Due date:
- * May 2, 2016 version: 1.0
+ * Project: AES Encryption and Decryption for 128, 192 and 256 bits 
+ * Due date: May 2, 2016 version: 1.0
  * 
  * This file contains the main method, it is an entry point to the program. It
  * calls AES encryption and decryption method based on the plainText or Cipher
